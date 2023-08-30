cf2ca96c9c185658faab69fcf35d1833e3e84e60
uhhJoyz <wbradford@renaissanceschool.org>
2023-08-30 12:40:54 -0400

    added command line execution support

---
+++ hs_solver.py
@@ -0,0 +1,11 @@
+import hitting_set_ilp as hilp
+import sys
+
+if len(sys.argv) != 3:
+    print(sys.argv)
+    raise Exception("This script requires two input arguments in the following order: "
+                    "\n 1. Input file \n 2. Output file");
+else:
+    desc = hilp.solve(str(sys.argv[1]))
+    with open(str(sys.argv[2]), 'w') as f:
+        f.write(str(desc))
